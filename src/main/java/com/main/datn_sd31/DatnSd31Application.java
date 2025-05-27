aea610e04b455aeb6d96df2dcf35ddd3001746d4
hungcoder03 <hungcoder03@gmail.com>
2025-05-27 19:36:56 +0700

    aaa

---
+++ src/main/java/com/main/datn_sd31/DatnSd31Application.java
@@ -0,0 +1,13 @@
+package com.main.datn_sd31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnSd31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnSd31Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/main/datn_sd31/DatnSd31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.datn_sd31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnSd31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
